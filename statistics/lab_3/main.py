30612a20618bca011a0dfc108ec3d884875c24cd
SavvaDaniil <savvadaniil25@gmail.com>
2024-11-25 11:20:26 +0300

    Переделывание лабораторной работы №3

--- statistics/lab_3/main.py
+++ statistics/lab_3/main.py
@@ -4,13 +4,20 @@ import numpy
 import matplotlib.pyplot as plt
 from scipy import stats
 
-data_general: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Генеральная совокупность")
-data_random: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Случайная выборка")
-data_stratified: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Белгородская область")
+#data_general: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Генеральная совокупность")
+#data_random: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Случайная выборка")
+#data_stratified: DataFrame = pd.read_excel('viborki.xlsx', header=0, sheet_name="Белгородская область")
+
+data_general: DataFrame = pd.read_csv('autos.csv')
 
 #print(data.head())
 
-COLUMN_NAME: str = "Количество мужчин и женщин"
+data_random: DataFrame = data_general.sample(frac=1/3, random_state=100)
+data_stratified: DataFrame = data_general.groupby('yearOfRegistration', group_keys=False).apply(lambda x: x.sample(frac=1/3, random_state=100))
+print(data_stratified.head())
+
+
+COLUMN_NAME: str = "price"
 
 # Среднего значения по выборкам
 print(f"Среднее значение (генеральная совокупность): {data_general[COLUMN_NAME].mean():.2f}")
@@ -20,9 +27,9 @@ print(f"Среднее значение (стратифицированная в
 
 def confidence_interval(data: DataFrame, confidence: float) -> tuple:
     """Расчет доверительного интервала"""
-    length = len(data)
-    mean = numpy.mean(data)
-    sem = stats.sem(data)  # standard error of the mean
+    length: int = len(data)
+    mean: float = numpy.mean(data)
+    sem = stats.sem(data)
     margin = sem * stats.t.ppf((1 + confidence) / 2, length - 1)
     return mean - margin, mean + margin
 
